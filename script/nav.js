ca4b38ead7fda9bc2c8eb458aaeb603259359e89
Aashutosh Kumar <95764284+Aashutosh-kr@users.noreply.github.com>
2022-02-27 21:49:56 +0530

    Menu btn background image path correction

--- script/nav.js
+++ script/nav.js
@@ -6,10 +6,10 @@ const nav = document.querySelector("nav");
 menubtn.addEventListener('click', () => {
     if (nav.id != 'nav-shown') {
         nav.id = 'nav-shown';
-        menubtn.style.backgroundImage = 'url(../img/icon-close.svg)';
+        menubtn.style.backgroundImage = 'url("../img/icon-close.svg")';
     } else {
         nav.id = 'nav-hidden';
-        menubtn.style.backgroundImage = 'url(../img/icon-hamburger.svg)';
+        menubtn.style.backgroundImage = 'url("../img/icon-hamburger.svg")';
     }
 })
 
