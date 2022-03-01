46f3cf70258ea1d0cd3b7b47d894a77aae55aa8e
Aashutosh <aashutoshkumar869@gmail.com>
2022-03-01 12:19:59 +0530

    almost complete

--- script/navigation.js
+++ script/navigation.js
@@ -13,30 +13,6 @@ menubtn.addEventListener('click', () => {
     }
 })
 
-const navlist = document.querySelectorAll('.primary-nav>li');
-Array.from(navlist).forEach(element => {
-    element.addEventListener('click', (e) => {
-        if (element.id == "") {
-            element.id = "active";
-            console.log(element.offsetLeft);
-        } else if (e.offsetLeft != element.offsetLeft) {
-            element.id = "";
-        }
-    })
-});
-
-
-// not working
-
-// if (nav.id == 'nav-shown') {
-//     document.addEventListener('click', () => {
-//         if (nav.id != 'nav-hidden') {
-//             nav.id = 'nav-hidden';
-//             menubtn.style.backgroundImage = 'url(../img/icon-hamburger.svg)';
-//             console.log('if');
-//         }
-//     })
-// }
 
 
 // For showing scroll to top button
@@ -49,3 +25,15 @@ document.addEventListener('scroll', () => {
         topbtn.style.display = "none";
     }
 })
+
+// not working properly
+
+if (nav.id == 'nav-shown') {
+    document.addEventListener('click', () => {
+        if (nav.id != 'nav-hidden') {
+            nav.id = 'nav-hidden';
+            menubtn.style.backgroundImage = 'url(../img/icon-hamburger.svg)';
+            console.log('if');
+        }
+    })
+}
